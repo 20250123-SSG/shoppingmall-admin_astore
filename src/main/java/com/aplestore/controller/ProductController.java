1f5e31fae009aff33952846307886f2ba8adb5c2
이가영 <rkduddl1717@gmail.com>
2025-05-19 19:54:47 +0900

    [Add] 상품등록 Controller 추가 author: 이가영
    상품등록서비스를 호출했을때,
    등록성공시에는 상품목록페이지로 리다이렉트시키고, 등록실패시에는 상품등록페이지로 포워딩시킨다.

--- src/main/java/com/aplestore/controller/ProductController.java
+++ src/main/java/com/aplestore/controller/ProductController.java
@@ -1,16 +1,47 @@
 package com.aplestore.controller;
 
+import com.aplestore.dto.ProductModelOptionDTO;
+import com.aplestore.dto.StoreDTO;
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.aplestore.service.ProductService;
 
+import java.util.List;
+
+@Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/product")
+@RequestMapping("/products")
 @Controller
 public class ProductController {
 
     private final ProductService productService;
+
+    @GetMapping("")
+    public String mainPage() {
+        return "products/list";  // 승주님 상품목록 페이지
+    }
+
+    @GetMapping("/regist")
+    public String registPage() {
+        return "products/regist";  // 승주님 상품목록 페이지
+    }
+
+    @PostMapping("/regist")
+    public String registProduct(ProductModelOptionDTO product) {
+        int result = productService.registProduct(product);
+        if (result == 0) {
+            // 등록실패메시지 출력
+            log.debug("다시 입력해주십시오. 등록페이지로 돌아갑니다.");
+            return "products/regist"; // 등록 페이지로 포워딩
+        }
+        log.debug("등록성공하였습니다. 목록페이지로 이동합니다.");
+        return "redirect:/products"; // 중복등록방지를 위해 redirect
+    }
 
 }
