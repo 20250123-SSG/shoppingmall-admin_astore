0089ec818a87dce508a61468e450c4651d0e65b0
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-05-19 19:02:58 +0900

    [Modify] CRUD 반영     - author : 이서연

--- src/main/java/com/aplestore/dao/UserMapper.java
+++ src/main/java/com/aplestore/dao/UserMapper.java
@@ -1,4 +1,19 @@
 package com.aplestore.dao;
 
+import com.aplestore.dto.UserDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
 public interface UserMapper {
+    List<UserDTO> getAllUsers();
+    UserDTO getUserById(int id);
+    void insertUser(UserDTO user);
+    void updateUser(UserDTO user);
+    void deleteUser(int id);
+
+    // 검색 기능 추가
+    List<UserDTO> searchUsers(@Param("keyword") String keyword, @Param("status") String status);
 }
