3545073ba736af6f8d4bcbdceb846a7e20c7eb74
이승주 <eun07213@gmail.com>
2025-05-19 18:05:14 +0900

    [Add]: 상품 목록 조회 서비스 추가 author: 이승주

--- src/main/java/com/aplestore/service/ProductService.java
+++ src/main/java/com/aplestore/service/ProductService.java
@@ -1,4 +1,11 @@
 package com.aplestore.service;
 
+import com.aplestore.dto.ProductModelDTO;
+import java.util.List;
+
 public interface ProductService {
+
+    List<ProductModelDTO> getAllModels(int offset, int limit, String keyword, String sort);
+    int countAllModels(String keyword);
+    List<String> suggestKeyword(String keyword);
 }
