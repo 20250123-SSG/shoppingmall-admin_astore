169773f8bb8f9cfa746d65d422052f824061fabb
seokbong <ssb7779@naver.com>
2025-05-20 11:54:56 +0900

    [Modify] 매퍼파일 위치 이동
    author: 송석봉
    BoardMapper => OrderMapper 이동

--- src/main/java/com/aplestore/dao/BoardMapper.java
+++ src/main/java/com/aplestore/dao/BoardMapper.java
@@ -1,12 +1,6 @@
 package com.aplestore.dao;
 
-import com.aplestore.dto.OrderDTO;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
-import java.util.Map;
 
 public interface BoardMapper {
-    List<Integer> selectAvailableYears();
-    List<OrderDTO> selectMonthlySales(@Param("year") int year);
+
 }
--- src/main/java/com/aplestore/dao/OrderMapper.java
+++ src/main/java/com/aplestore/dao/OrderMapper.java
@@ -1,4 +1,12 @@
 package com.aplestore.dao;
 
+import com.aplestore.dto.OrderDTO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface OrderMapper {
+    List<Integer> selectAvailableYears();
+
+    List<OrderDTO> selectMonthlySales(@Param("year") int year);
 }
