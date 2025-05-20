af8343b69bad3333fca5147f05ae020d32a96a32
seokbong <ssb7779@naver.com>
2025-05-20 11:57:49 +0900

    [Modify] 서비스 파일 이동 및 수정
    author: 송석봉
    BoardService,Serviceimpl => OrderService,ServiceImpl 파일 이동 및 수정

--- src/main/java/com/aplestore/service/BoardService.java
+++ src/main/java/com/aplestore/service/BoardService.java
@@ -1,11 +1,5 @@
 package com.aplestore.service;
 
-import com.aplestore.dto.OrderDTO;
-
-import java.util.List;
-import java.util.Map;
-
 public interface BoardService {
-    List<Integer> getAvailableYears();
-    List<OrderDTO> getMonthlySalesByYear(int year);
+
 }
--- src/main/java/com/aplestore/service/BoardServiceImpl.java
+++ src/main/java/com/aplestore/service/BoardServiceImpl.java
@@ -1,45 +1,6 @@
 package com.aplestore.service;
 
-import com.aplestore.dao.BoardMapper;
-import com.aplestore.dto.OrderDTO;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-@Service
 public class BoardServiceImpl implements BoardService {
 
-    private final BoardMapper boardMapper;
 
-    public BoardServiceImpl(BoardMapper boardMapper) {
-        this.boardMapper = boardMapper;
-    }
-
-    @Override
-    public List<Integer> getAvailableYears() {
-        return boardMapper.selectAvailableYears();
-    }
-
-    @Override
-    public List<OrderDTO> getMonthlySalesByYear(int year) {
-        List<OrderDTO> dbSales = boardMapper.selectMonthlySales(year);
-
-        Map<Integer, Integer> monthToSales = new HashMap<>();
-        for (OrderDTO m : dbSales) {
-            monthToSales.put(m.getMonth(), m.getTotalSales());
-        }
-
-        List<OrderDTO> fullSales = new ArrayList<>();
-        for (int month = 1; month <= 12; month++) {
-            OrderDTO m = new OrderDTO();
-            m.setMonth(month);
-            m.setTotalSales(monthToSales.getOrDefault(month, 0));
-            fullSales.add(m);
-        }
-
-        return fullSales;
-    }
 }
